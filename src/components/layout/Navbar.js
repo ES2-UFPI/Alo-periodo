65bca0d73caf1f9b4f0faea57a625e98c3d76d69
mtb <mateustitob@hotmail.com>
2019-04-30 16:53:43 -0300

    Bugs fixed Issue #15 (Iniciais do Nome)

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -6,9 +6,9 @@ import { connect } from 'react-redux'
 
 const Navbar = (props) => {
   
-  const { auth } = props;
+  const { auth, profile } = props;
   // console.log(auth);
-  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
+  const links = auth.uid ? <SignedInLinks profile={ profile } /> : <SignedOutLinks />;
   // se o user logar (autenticado pelo uid) tem acesso ao New File (SignedInLinks)
   // se nao tiver logado, soh aparece SignedOutLinks
   
@@ -24,7 +24,8 @@ const Navbar = (props) => {
 
 const mapStateToProps = (state) => {
   return {
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    profile: state.firebase.profile
   }
 }
 
--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -2,21 +2,16 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { signOut } from '../../store/actions/authActions'
-import { firestoreConnect } from 'react-redux-firebase'
-import { compose } from 'redux'
 
 const SignedInLinks = (props) => {  // Links mostrados ao usuario logado
-
-  const { project } = props;
-
-  const user_logo = project.authorFirstName[0] + project.authorLastName[0]  // Praticamente isso que deve ser feito
-
   return (
     <div>
       <ul className="right">
         <li><NavLink to='/create'>New File</NavLink></li>
-        <li><a onClick={props.signOut}>Log Out</a></li>
-        <li><button id='btn-user-logo' className="btn btn-floating gray darken-3">{ user_logo }</button></li>
+        <li><a onClick={ props.signOut }>Log Out</a></li>
+        <li><button id='btn-logo' className="btn btn-floating green darken-2">
+          { props.profile.initials }
+        </button></li>
       </ul>
     </div>
   )
@@ -28,19 +23,4 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-const mapStateToProps = (state, ownProps) => {
-  // console.log(state);
-  const id = ownProps.match.params.id;
-  const projects = state.firestore.data.projects;
-  const project = projects ? projects[id] : null
-  return {
-    project: project
-  }
-}
-
-export default compose(
-    connect(mapStateToProps, mapDispatchToProps), // Antes tinha 2 linhas com connect
-    firestoreConnect([
-      { collection: 'projects' }
-    ])
-  )(SignedInLinks)
+export default connect(null, mapDispatchToProps)(SignedInLinks)
