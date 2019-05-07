b02775b08fdfc957f9b64fb0d47583b50105861e
Hilderlan <hilakt9698@gmail.com>
2019-05-07 03:12:00 -0300

    Issue #22 resolvida, falta apenas ajeitar mais o layout.

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,26 +1,63 @@
-import React from 'react'
+import React from 'react';
 import { NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { signOut } from '../../store/actions/authActions'
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
-const SignedInLinks = (props) => {  // Links mostrados ao usuario logado
-  return (
-    <div>
-      <ul className="right">
-        <li><NavLink to='/create'>New File</NavLink></li>
-        <li><a onClick={ props.signOut }>Log Out</a></li>
-        <li><button id='btn-logo' className="btn btn-floating green darken-2">
-          { props.profile.initials }
-        </button></li>
-      </ul>
-    </div>
-  )
+class SignedInLinks extends React.Component {
+  state = {
+    anchorEl: null,
+  };
+
+  handleClick = event => {  // Abre o menu quando clicado
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => { // Fecha o menu apos alguma opcao ter sido selecionada
+    this.setState({ anchorEl: null });
+  };
+
+  render() {
+    const { anchorEl } = this.state;
+    const props = this.props; // Pega as propriedades da pag
+
+    return(
+      <div>
+        <ul className="right">
+          <li><NavLink to='/create'>New File</NavLink></li>
+          <li>
+            <div className="btn btn-floating green darken-2">
+              <Button
+                aria-owns={anchorEl ? 'simple-menu' : undefined}
+                aria-haspopup="true"
+                onClick={this.handleClick}
+              >
+              { props.profile.initials }
+              </Button>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={this.handleClose}
+              >
+                <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                <MenuItem onClick={this.handleClose}>Change Password</MenuItem>
+                <MenuItem onClick={props.signOut}>Logout</MenuItem>
+              </Menu>
+            </div>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    signOut: () => dispatch(signOut())
-  }
+    return {
+      signOut: () => dispatch(signOut())
+    }
 }
 
 export default connect(null, mapDispatchToProps)(SignedInLinks)
