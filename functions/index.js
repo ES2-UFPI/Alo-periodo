68208fae63c06273f8a82af8a21fb50879d3e6b0
mtb <mateustitob@hotmail.com>
2019-06-05 23:18:57 -0300

    primeiro test server

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,14 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions'); // todas as funçoes do firebase sao controladas por essa const
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+// control de requisiçoes HTTPs
+// tradicionais request / response ...
+    // Response: servidor --> navegador
+    // Request: navegador --> servidor
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+    // toda requisiçao vai ficando salva no database nas "Functions"
+});
